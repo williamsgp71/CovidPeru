95c29802d57f7d1d1ef7e843b1835f486f36547b
williamsgp71 <45520681+williamsgp71@users.noreply.github.com>
2021-01-27 16:08:15 -0500

    Update CovidPeruV1.py
    avance

--- CovidPeruV1.py
+++ CovidPeruV1.py
@@ -29,5 +29,19 @@ st.map(map_data)
 
 if st.checkbox('Show dataframe'):
     chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+import time
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
